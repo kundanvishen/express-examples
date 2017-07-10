f82b5ea76c859d984d5b7a4187f0b8005d7f6138
Kundan Singh <kundan.singh979@gmail.com>
2017-07-10 18:04:11 +0530

    Template paramters example

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+// to specify another folder other than views
+// app.set('views', __dirname + '/templates');
+
+// app.get('/', function(req, res) {
+//     res.render('default');
+// });
+
+// Example to pass parameters to templates
 app.get('/', function(req, res) {
-    res.render('default');
+    res.render('default', {
+        title: 'Home',
+        users: ['John', 'Henry', 'Mark']
+    });
 });
 
 app.listen(3000, function(req, res) {
