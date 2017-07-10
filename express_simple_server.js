1daf929b87c9d65385aaf92f159bd61d2425e32f
Kundan Singh <kundan.singh979@gmail.com>
2017-07-10 17:44:56 +0530

    Query parameter example

--- express_simple_server.js
+++ express_simple_server.js
@@ -9,6 +9,18 @@ app.get('/', function(req, res) {
     res.sendFile('index.html', { root: path.join(__dirname + '/views') });
 });
 
+app.get('/who/:name?/:title?', function(req, res) {
+    var name = req.params.name;
+    var title = req.params.title;
+
+    res.send('<p>Name: ' + name + '</p><p>Title: ' + title + '</p>');
+});
+
+
+app.get('*', function(req, res) {
+    res.send('Probably you are lost!');
+});
+
 app.listen(port, function() {
     console.log('Server is listening at port: ' + port);
 });
